9e2c04ffd391c82e9f73bbc6634d11b5e73946b5
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-02-25 19:01:02 +0800

    add connection to database & add users api route

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const mysql = require('mysql2');
-const validator = require('./validate');
 
 const app = express();
 
+const connection = mysql.createConnection({
+  host: 'database-instance.c1a2hycmc2k6.ap-northeast-1.rds.amazonaws.com',
+  database: 'assignment',
+  user: 'admin',
+  password: 'snowham1949'
+});
+
 app.listen(3000);
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.use('/healthcheck', require('./routes/healthcheck'));
+app.use('/users', require('./routes/users'));
