81429ebb9991e1570aed6b5bf9ef7997e476327f
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-02-25 21:08:57 +0800

    remove database connection

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const express = require('express');
-const mysql = require('mysql2');
 
 const app = express();
-
-const connection = mysql.createConnection({
-  host: 'database-instance.c1a2hycmc2k6.ap-northeast-1.rds.amazonaws.com',
-  database: 'assignment',
-  user: 'admin',
-  password: 'snowham1949'
-});
 
 app.listen(3000);
 
