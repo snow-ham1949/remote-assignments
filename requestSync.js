2012d3059f4ed259585e55cfc65fea345661500f
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-02-26 19:52:05 +0800

    update with time calculation

--- requestSync.js
+++ requestSync.js
@@ -3,8 +3,11 @@ const url = "https://api.appworks-school-campus3.online/api/v1/clock/delay";
 var request = require('sync-request');
 
 function requestSync(url) {
+  const startTime = Date.now();
   var res = request('GET', url);
-  console.log(res.getBody());
+  let currentTime = JSON.parse(res.getBody()).data.now;
+  currentTime /= 1000000;
+  console.log(`${currentTime - startTime}`);
 }
 requestSync(url) // would print out the execution time
 requestSync(url)
