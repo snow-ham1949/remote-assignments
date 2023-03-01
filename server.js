bb6296b064663bc5161ff661fe4feb1ee93db87c
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-03-01 16:50:47 +0800

    update server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+app.listen(5000);
 
 app.use('/healthcheck', require('./routes/healthcheck'));
 app.use('/users', require('./routes/users'));
