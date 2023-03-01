897af4bbfbcb7387649e658fc052431ddccd3569
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-03-01 22:19:40 +0800

    handle correct situation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,56 @@
 import './App.css';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const App = () => {
-  const handleSubmit = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [userID, setUserID] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
     axios({
       method: 'post',
       url: 'http://localhost:4000/users',
-      headers: { 
+      headers: {
         'content-type': 'application/json',
-        'request-date': 'Wed, 22 Feb 2023 16:47:36 GMT'
-      }, 
-      data: {
-        name: "erin",
-        email: "te@appworks.com",
-        password: "test_123", // This is the body part
-      }
+        'request-date': new Date().toUTCString()
+      },
+      data: { name, email, password }
     })
-    .then((res) => {console.log(res.data.data.user.id)});
+      .then((res) => { setUserID(res.data.data.user.id) })
+      .catch(function (error) {
+        console.log("error!" + error.toJSON());
+      });
   };
+
   return (
     <div className="container">
-      <form id="form" className="form">
+      <form id="form" className="form" onSubmit={handleSubmit}>
+        {userID &&
+          <>
+            <h4 className="success-message">
+              Successfully Sign Up.
+            </h4>
+            <h4 className="success-message">
+              User ID: {userID}
+            </h4>
+          </>
+        }
         <div className="form-control">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" placeholder="Enter username" />
+          <label htmlFor="name">Username</label>
+          <input type="text" id="name" value={name} placeholder="Enter name" onChange={(e) => setName(e.target.value)} />
         </div>
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" placeholder="Enter email" />
+          <input type="text" id="email" value={email} placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" placeholder="Enter password" />
+          <input type="password" id="password" value={password} placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} />
         </div>
-        <button type="submit" onClick={handleSubmit}>Submit</button>
+        <button type="submit">Sign Up</button>
       </form>
     </div>
   );
