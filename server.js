0744afb6076d9b33ba9d4fd46f319f3403396388
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-03-02 13:18:59 +0800

    server build

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, './frontend/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './frontend/build', 'index.html'));
+});
+
 app.listen(4000);
 
 app.use('/healthcheck', require('./routes/healthcheck'));
